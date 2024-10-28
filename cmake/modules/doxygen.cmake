600e8d17903351994db2e8e14721ff60fd2408d3
FabianHaeusel <75080349+FabianHaeusel@users.noreply.github.com>
2024-10-28 16:30:41 +0100

    added doxygen module with build target

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,17 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
-# TODO: create CMake build option for the target.
+find_package(Doxygen)
+if (DOXYGEN_FOUND)
+    set(DOXYFILE_IN ${PROJECT_SOURCE_DIR}/Doxyfile)
 
-# TODO: Add a custom target for building the documentation.
+    configure_file(${DOXYFILE_IN} ${DOXYFILE_OUT} @ONLY)
+
+    add_custom_target(doc_doxygen
+            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_IN}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMENT "Building doxygen documentation"
+            VERBATIM
+    )
+else (DOXYGEN_FOUND)
+    message("Doxygen is not installed")
+endif (DOXYGEN_FOUND)
