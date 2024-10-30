595f1c42302e3f99a25e930cf52b657e3114c0f2
Anton Scheitler <anton.scheitler@gmail.com>
2024-10-30 18:57:53 +0100

    Gravity computation optimized

--- src/computations/forces/Forces.cpp
+++ src/computations/forces/Forces.cpp
@@ -1,34 +1,27 @@
 #include <array>
 #include <cmath>
+#include <functional>
 
 #include "../../utils/ArrayUtils.h"
 #include "particle/ParticleContainer.h"
 
 namespace forces {
-    void computeGravitySinglePlanet(Particle &planet, ParticleContainer &planets);
-
     void computeGravity(ParticleContainer &planets) {
-        for (Particle &planet: planets) {
-            computeGravitySinglePlanet(planet, planets);
-        }
-    }
-
-    void computeGravitySinglePlanet(Particle &planet, ParticleContainer &planets) {
-        std::array<double, 3> newForce = {0, 0, 0};
-        planet.setOldF(planet.getF());
-
-        for (Particle &otherPlanet: planets) {
-            if (planet == otherPlanet) {
-                continue;
-            }
-            std::array<double, 3> distanceVector = ArrayUtils::elementWisePairOp(otherPlanet.getX(), planet.getX(),
-                    std::minus<>());
-            double coefficient = (planet.getM() * otherPlanet.getM()) / std::pow(ArrayUtils::L2Norm(distanceVector), 3);
-            std::array<double, 3> partialNewForce = ArrayUtils::elementWiseScalarOp(coefficient, distanceVector,
-                    std::multiplies<>());
-            newForce = ArrayUtils::elementWisePairOp(newForce, partialNewForce, std::plus<>());
+        for (Particle& planet: planets) {
+            planet.setOldF(planet.getF());
+            planet.setF({0, 0, 0});
         }
 
-        planet.setF(newForce);
+        for (auto it = planets.beginPairParticle(); it != planets.endPairParticle(); ++it) {
+            std::pair<Particle&, Particle&> pair = *it;
+            std::array<double, 3> newForce = {0, 0, 0};
+
+            std::array<double, 3> distanceVector = ArrayUtils::elementWisePairOp(pair.second.getX(), pair.first.getX(), std::minus<>());
+            double coefficient = (pair.first.getM() * pair.second.getM()) / std::pow(ArrayUtils::L2Norm(distanceVector), 3);
+            newForce = ArrayUtils::elementWiseScalarOp(coefficient, distanceVector, std::multiplies<>());
+            pair.first.setF(ArrayUtils::elementWisePairOp(pair.first.getF(), newForce, std::plus<>()));
+            std::array<double, 3> revNewForce = ArrayUtils::elementWiseScalarOp(-1, newForce, std::multiplies<>());
+            pair.second.setF(ArrayUtils::elementWisePairOp(pair.second.getF(), revNewForce, std::plus<>()));
+        }
     }
 }
