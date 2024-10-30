13b7cbcb4285634517877c2f094550d06bc68cf5
FabianHaeusel <hfabi2255@gmail.com>
2024-10-30 12:03:04 +0100

    rewrote particlecontainer class and header with new iterators

--- src/particle/ParticleContainer.cpp
+++ src/particle/ParticleContainer.cpp
@@ -1,26 +1,95 @@
+#include <list>
 #include "ParticleContainer.h"
 
-ParticleContainer::ParticleContainer() {
-    this->particles = std::vector<Particle>{};
-}
-ParticleContainer::ParticleContainer(const std::vector<Particle>& particles) {
-    this->particles = particles;
-}
+class ParticleContainer {
 
-void ParticleContainer::addParticle(const Particle &particle) {
-    this->particles.push_back(particle);
-}
+private:
 
-void ParticleContainer::removeParticle(Particle &particle) {
-    //TODO
-}
+    std::vector<Particle> particles;
 
-Iterator ParticleContainer::begin() {
-    if(particles.size() == 0) return nullptr;
-    return Iterator(&this->particles.at(0));
-}
-Iterator ParticleContainer::end() {
-    if(particles.size() == 0) return nullptr;
-    return Iterator(&this->particles.at(this->particles.size() - 1));
-}
+public:
 
+    ParticleContainer() {
+        this->particles = std::vector<Particle>{};
+    }
+
+    ParticleContainer(const std::vector<Particle> &particles) {
+        this->particles = particles;
+    }
+
+    void addParticle(const Particle &particle) {
+        this->particles.push_back(particle);
+    }
+
+
+    class Iterator : public std::iterator<std::forward_iterator_tag, Particle> {
+
+    public:
+        Iterator(std::vector<Particle>::iterator it) : it(it) {}
+
+        Iterator& operator++() {
+            ++it;
+            return *this;
+        }
+
+        bool operator!=(const Iterator &other) const { return it != other.it; }
+
+        Particle &operator*() { return *it; }
+
+    private:
+        std::vector<Particle>::iterator it;
+
+    };
+
+    Iterator begin() { return Iterator(particles.begin()); }
+
+    Iterator end() { return Iterator(particles.end()); }
+
+    class PairIterator : std::iterator<std::forward_iterator_tag, std::pair<Particle, Particle>> {
+
+    public:
+        PairIterator(std::vector<Particle>::iterator it1, std::vector<Particle>::iterator it2) :
+                it1(it1), it2(it2) {
+            nextPair();
+        }
+
+        PairIterator &operator++() {
+            ++it2;
+            if (it2 == particles.end()) {
+                ++it1;
+                it2 = std::next(it1);
+            }
+            nextPair();
+            return *this;
+        }
+
+        bool operator!=(const PairIterator &other) const {
+            return it1 != other.it1 || it2 != other.it2;
+        }
+
+        std::pair<Particle, Particle> operator*() const {
+            return std::make_pair(*it1, *it2);
+        }
+
+    private:
+
+        void nextPair() {
+            while (it2 != particles.end() && it1 == it2) {
+                ++it2;
+            }
+        }
+
+        std::vector<Particle>::iterator it1, it2;
+        std::vector<Particle> particles;
+
+        PairIterator pairBegin() {
+            return PairIterator(particles.begin(), std::next(particles.begin()));
+        }
+
+        PairIterator pairEnd() {
+            return PairIterator(particles.end(), particles.end());
+        }
+
+    };
+
+};
