81bac6c93c9d5606cce193331ebbb33f2679a59a
FabianHaeusel <hfabi2255@gmail.com>
2024-11-08 13:34:24 +0100

    changed spdlog include

--- cmake/modules/spdlog.cmake
+++ cmake/modules/spdlog.cmake
@@ -1,14 +1,20 @@
-include(ExternalProject)
-
-ExternalProject_Add(spdlog
-        PREFIX spdlog
-        SOURCE_DIR ${PROJECT_SOURCE_DIR}/libs/spdlog
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-        -DCMAKE_INSTALL_PREFIX=${STAGING_DIR}
-        -DSPDLOG_BUILD_SHARED=OFF
+include(FetchContent)
+FetchContent_Declare(
+        spdlog
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_TAG v1.14.1
 )
+FetchContent_MakeAvailable(spdlog)
 
-add_dependencies(MolSim spdlog)
+#ExternalProject_Add(spdlog
+#        PREFIX spdlog
+#        SOURCE_DIR ${PROJECT_SOURCE_DIR}/libs/spdlog
+#        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+#        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+#        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+#        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+#        -DCMAKE_INSTALL_PREFIX=${STAGING_DIR}
+#        -DSPDLOG_BUILD_SHARED=OFF
+#)
+#
+#add_dependencies(MolSim spdlog)
