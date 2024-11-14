8cabb36b021ac0aa714e7a28ba4e81a2ecb0fac6
FabianHaeusel <hfabi2255@gmail.com>
2024-11-14 21:52:30 +0100

    changed doc and function names in filereadertest

--- test/inputReader/FileReaderTest.cpp
+++ test/inputReader/FileReaderTest.cpp
@@ -18,7 +18,7 @@ protected:
  * @param x1 value 1
  * @param x2 value 2
  */
-void assertArray(std::array<double, 3> v, double x0, double x1, double x2) {
+void expectArrayCorrect(std::array<double, 3> v, double x0, double x1, double x2) {
     EXPECT_DOUBLE_EQ(v[0], x0);
     EXPECT_DOUBLE_EQ(v[1], x1);
     EXPECT_DOUBLE_EQ(v[2], x2);
@@ -28,25 +28,25 @@ void assertArray(std::array<double, 3> v, double x0, double x1, double x2) {
  * @brief checks if all particles have the same position, velocity and mass as specified in the test input file
  * @param p particleContainer containing the particles
  */
-void assertParticlesCometSim(ParticleContainer &p) {
+void expectParticlesCometSimCorrect(ParticleContainer &p) {
     // particle 0
-    assertArray(p.getParticle(0).getX(), 0, 0, 0);
-    assertArray(p.getParticle(0).getV(), 0, 0, 0);
+    expectArrayCorrect(p.getParticle(0).getX(), 0, 0, 0);
+    expectArrayCorrect(p.getParticle(0).getV(), 0, 0, 0);
     EXPECT_DOUBLE_EQ(p.getParticle(0).getM(), 1.0);
 
     // particle 1
-    assertArray(p.getParticle(1).getX(), 0, 1, 0);
-    assertArray(p.getParticle(1).getV(), -1, 0, 0);
+    expectArrayCorrect(p.getParticle(1).getX(), 0, 1, 0);
+    expectArrayCorrect(p.getParticle(1).getV(), -1, 0, 0);
     EXPECT_DOUBLE_EQ(p.getParticle(1).getM(), 3.0e-6);
 
     // particle 2
-    assertArray(p.getParticle(2).getX(), 0.0, 5.36, 0.0);
-    assertArray(p.getParticle(2).getV(), -0.425, 0.0, 0.0);
+    expectArrayCorrect(p.getParticle(2).getX(), 0.0, 5.36, 0.0);
+    expectArrayCorrect(p.getParticle(2).getV(), -0.425, 0.0, 0.0);
     EXPECT_DOUBLE_EQ(p.getParticle(2).getM(), 9.55e-4);
 
     // particle 3
-    assertArray(p.getParticle(3).getX(), 34.75, 0.0, 0.0);
-    assertArray(p.getParticle(3).getV(), 0.0, 0.0296, 0.0);
+    expectArrayCorrect(p.getParticle(3).getX(), 34.75, 0.0, 0.0);
+    expectArrayCorrect(p.getParticle(3).getV(), 0.0, 0.0296, 0.0);
     EXPECT_DOUBLE_EQ(p.getParticle(3).getM(), 1.0e-14);
 }
 
@@ -54,27 +54,27 @@ void assertParticlesCometSim(ParticleContainer &p) {
  * @brief checks if all particles have the same position, velocity and mass as specified in the test input file
  * @param p particleContainer containing the particles
  */
-void assertParticlesCollisionSim(ParticleContainer &p) {
+void expectParticlesCollisionSimCorrect(ParticleContainer &p) {
     // cuboid 0
     // particle 0
-    assertArray(p.getParticle(0).getX(), 0, 0, 0);
-    assertArray(p.getParticle(0).getV(), 0, 0, 7.4);
+    expectArrayCorrect(p.getParticle(0).getX(), 0, 0, 0);
+    expectArrayCorrect(p.getParticle(0).getV(), 0, 0, 7.4);
     EXPECT_DOUBLE_EQ(p.getParticle(0).getM(), 1.0);
 
     // particle 1
-    assertArray(p.getParticle(1).getX(), 1.1225, 0, 0);
-    assertArray(p.getParticle(1).getV(), 0, 0, 7.4);
+    expectArrayCorrect(p.getParticle(1).getX(), 1.1225, 0, 0);
+    expectArrayCorrect(p.getParticle(1).getV(), 0, 0, 7.4);
     EXPECT_DOUBLE_EQ(p.getParticle(1).getM(), 1);
 
     // cuboid 1
     // particle 0
-    assertArray(p.getParticle(2).getX(), 15, 15, 0);
-    assertArray(p.getParticle(2).getV(), 0, -10, 0);
+    expectArrayCorrect(p.getParticle(2).getX(), 15, 15, 0);
+    expectArrayCorrect(p.getParticle(2).getV(), 0, -10, 0);
     EXPECT_DOUBLE_EQ(p.getParticle(2).getM(), 3);
 
     // particle 1
-    assertArray(p.getParticle(3).getX(), 15, 16.1225, 0);
-    assertArray(p.getParticle(3).getV(), 0.0, -10, 0);
+    expectArrayCorrect(p.getParticle(3).getX(), 15, 16.1225, 0);
+    expectArrayCorrect(p.getParticle(3).getV(), 0.0, -10, 0);
     EXPECT_DOUBLE_EQ(p.getParticle(3).getM(), 3);
 }
 
@@ -88,7 +88,7 @@ void FileReaderTest::testCometSim(char *inputFile) {
     inputReader::FileReader fileReader = inputReader::FileReader(simulationData);
     ParticleContainer p{};
     fileReader.readFile(p, inputFile);
-    assertParticlesCometSim(p);
+    expectParticlesCometSimCorrect(p);
 }
 
 /**
@@ -101,7 +101,7 @@ void FileReaderTest::testCollisionSim(char *inputFile) {
     inputReader::FileReader fileReader = inputReader::FileReader(simulationData);
     ParticleContainer p{};
     fileReader.readFile(p, inputFile);
-    assertParticlesCollisionSim(p);
+    expectParticlesCollisionSimCorrect(p);
 }
 
 /**
@@ -119,14 +119,14 @@ TEST_F(FileReaderTest, TxtFileReaderCometSimInputTest) {
 }
 
 /**
- * @brief tests the jsonFileReader for comet simulation input file
+ * @brief tests the jsonFileReader for cuboid collision simulation input file
  */
 TEST_F(FileReaderTest, JsonFileReaderCollisionSimInputTest) {
     testCollisionSim((char *) "../../testInput/test-input-cuboids.json");
 }
 
 /**
- * @brief tests the txtFileReader for comet simulation input file
+ * @brief tests the txtFileReader for cuboid collision simulation input file
  */
 TEST_F(FileReaderTest, TxtFileReaderCollisionSimInputTest) {
     testCollisionSim((char*) "../../testInput/test-input-cuboids.txt");
