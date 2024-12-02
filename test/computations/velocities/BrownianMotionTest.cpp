9123a516291c7d23f257b42298c2f1725ebf54c0
FabianHaeusel <hfabi2255@gmail.com>
2024-12-02 20:24:51 +0100

    added test for brownianMotion

---
+++ test/computations/velocities/BrownianMotionTest.cpp
@@ -0,0 +1,52 @@
+#include "../../../../src/particle/container/ParticleContainerDirectSum.h"
+#include "../../../../src/computations/positions/PositionComputations.h"
+#include <gtest/gtest.h>
+#include "../../../../src/computations/velocities/VelocityComputations.h"
+
+/**
+ * @brief Tests the position computations (src/computations/positions) via analytically calculated examples
+ */
+class BrownianMotionTest : public testing::Test {
+
+protected:
+    void SetUp() override {};
+};
+
+/**
+ * @brief test if the brownian motion velocity applied in 2D is in the correct range for x and y coords,
+ * and the z-velocity is not changed
+ */
+TEST_F(BrownianMotionTest, BrownianMotion2DTest) {
+    double averageVelocity = 0.1;
+
+    Particle particle{{0,0,0}, {1.0, 1.0, 10.0}, 1, 0};
+    std::vector<Particle> particles{particle};
+    ParticleContainerDirectSum container{particles};
+
+    VelocityComputations::applyBrownianMotion2D(container, averageVelocity);
+
+    Particle& p = container.getParticle(0);
+
+    EXPECT_TRUE(p.getV()[0] >= 1 - 5 * averageVelocity && p.getV()[0] <= 1 + 5 * averageVelocity);
+    EXPECT_TRUE(p.getV()[1] >= 1 - 5 * averageVelocity && p.getV()[1] <= 1 + 5 * averageVelocity);
+    EXPECT_FLOAT_EQ(p.getV()[2], 10.0);
+}
+
+/**
+ * @brief checks if the brownian motion velocity applied in 3D is in the correct range for x, y and z coords
+ */
+TEST_F(BrownianMotionTest, BrownianMotion3DTest) {
+    double averageVelocity = 0.1;
+
+    Particle particle{{0,0,0}, {1.0, 1.0, 1.0}, 1, 0};
+    std::vector<Particle> particles{particle};
+    ParticleContainerDirectSum container{particles};
+
+    VelocityComputations::applyBrownianMotion3D(container, averageVelocity);
+
+    Particle& p = container.getParticle(0);
+
+    EXPECT_TRUE(p.getV()[0] >= 1 - 5 * averageVelocity && p.getV()[0] <= 1 + 5 * averageVelocity);
+    EXPECT_TRUE(p.getV()[1] >= 1 - 5 * averageVelocity && p.getV()[1] <= 1 + 5 * averageVelocity);
+    EXPECT_TRUE(p.getV()[2] >= 1 - 5 * averageVelocity && p.getV()[2] <= 1 + 5 * averageVelocity);
+}
